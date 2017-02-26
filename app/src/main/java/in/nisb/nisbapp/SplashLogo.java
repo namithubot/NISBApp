f414603a7863811c6bfe592624b857ef62ee7c33
namithubot <namithubot@gmail.com>
2017-02-26 20:20:30 +0530

    Initial commit

---
+++ app/src/main/java/in/nisb/nisbapp/SplashLogo.java
@@ -0,0 +1,13 @@
+package in.nisb.nisbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashLogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_logo);
+    }
+}
