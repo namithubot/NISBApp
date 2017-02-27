1ada0deb68d89c8cd12ce3191eda9085b68f2972
Abhinav_Kumar <abhinav395.ak@gmail.com>
2017-02-27 10:31:42 +0530

    Added splash and login screen

--- app/src/main/java/in/nisb/nisbapp/SplashLogo.java
+++ app/src/main/java/in/nisb/nisbapp/SplashLogo.java
@@ -1,5 +1,6 @@
 package in.nisb.nisbapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,30 @@ public class SplashLogo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_logo);
+
+        Thread splashThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try {
+                    int waited = 0;
+                    while (waited < 1533)
+                    {
+                        sleep(100);
+                        waited += 100;
+                    }
+                } catch (InterruptedException e)
+                {
+                    // do nothing
+                } finally
+                {
+                    Intent i = new Intent(SplashLogo.this,LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        splashThread.start();
     }
 }
